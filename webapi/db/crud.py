e993726c8c9100fc1e077841824efa1d81b71153
chdxia <chdxiamail@gmail.com>
2022-05-09 18:35:40 +0800

    update db.crud

--- webapi/db/crud.py
+++ webapi/db/crud.py
@@ -27,6 +27,14 @@ def get_items(db: Session, skip: int =0, limit: int =100):
     return db.query(models.Item).offset(skip).limit(limit).all()
 
 
+def get_item_by_id(db: Session, item_id= int):
+    return db.query(models.Item).filter(models.Item.id == item_id).first()
+
+
+def get_items_by_userid(db: Session, user_id= int):
+    return db.query(models.Item).filter(models.Item.owner_id == user_id).all()
+
+
 def create_user_item(db: Session, item: schemas.ItemCreate, user_id: int):
     db_item = models.Item(**item.dict(), owner_id =user_id)
     db.add(db_item)
