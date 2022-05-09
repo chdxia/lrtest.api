a40d09c0b9fec66435bbc94b0065593bba1c75ac
chdxia <chdxiamail@gmail.com>
2022-05-09 19:28:01 +0800

    update routers.items

--- webapi/routers/items.py
+++ webapi/routers/items.py
@@ -19,11 +19,12 @@ async def read_items(skip: int=0, limit: int=100, db: Session=Depends(get_db)):
     return items
 
 
-@router.get("/{item_id}")
-async def read_item(item_id: str):
-    if item_id not in fake_items_db:
+@router.get("/{item_id}", response_model=schemas.Item)
+async def read_item(item_id: int, db: Session=Depends(get_db)):
+    db_item= crud.get_item_by_id(db, item_id= item_id)
+    if item_id is None:
         raise HTTPException(status_code=404, detail="Item not found")
-    return {"name": fake_items_db[item_id]["name"], "item_id": item_id}
+    return db_item
 
 
 @router.put(
