4f49186b3263c9fa7a1c7386e092135e311aeaeb
chdxia <chdxiamail@gmail.com>
2022-05-28 08:02:37 +0800

    修复db_connection空闲8小时后，被db_server断开连接，从而导致api请求报错的问题

--- app/db/database.py
+++ app/db/database.py
@@ -7,7 +7,11 @@ from ..utils.config import GetConfig
 SQLALCHEMY_DATABASE_URL = GetConfig.get_database_url()
 
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+#  创建一个带连接池的引擎，，pool_recycle=7200参数
+#  * db_server默认空闲8小时断开connection
+#  * pool_recycle=7200参数表示，connection空闲7200秒，自动重新获取
+#  * 如果使用poolclass=NullPool参数将禁用连接池，关闭会话后立即断开数据库连接！！！
+engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_recycle=7200)
 
 
 SessionLocal =  sessionmaker(autocommit=False, autoflush=False, bind=engine)
@@ -17,6 +21,7 @@ def get_db():
     try:
         yield db
     finally:
+        # 关闭会话
         db.close()
 
 
