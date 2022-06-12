fbe84a6492e085761bcce3eb15df849e13698c25
chdxia <chdxiamail@gmail.com>
2022-06-13 07:24:29 +0800

    no message

--- app/dependencies.py
+++ app/dependencies.py
@@ -7,5 +7,5 @@ from .db.database import get_db
 async def get_token_header(X_Token: str = Header(...), db: Session=Depends(get_db)):
     db_user = crud.get_users(db, access_token=X_Token)
     # 万能token:123456
-    if len(db_user) == 0 & X_Token != '123456':
+    if len(db_user) == 0 and X_Token != '123456':
         raise HTTPException(status_code=400, detail="X-Token header invalid")
