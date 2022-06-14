3ce6b5eb0e12ed6a675ec453097f7a9defa55de6
chdxia <chdxiamail@gmail.com>
2022-06-15 07:57:42 +0800

    gunicorn开启守护进程，将gunicorn与控制终端分离并进入后台

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -12,6 +12,7 @@ bind = '0.0.0.0:8082'
 # Server最大连接数（64-2048）
 backlog = 2048
 
+
 # Worker processes
 # Server的工作进程数,正常范围:2-4 x $(NUM_CORES)
 workers = 2
@@ -24,15 +25,17 @@ timeout = 30
 # normal:1-5
 keepalive = 2
 
+
 # Debugging
 # 调试功能
 debug = False
 # 输出运行时的执行结果
 spew = False
 
+
 # Server mechanics
-# 开启守护进程,将主进程(gunicorn)与控制进程分离
-daemon = False
+# 开启守护进程,将主进程(gunicorn)与控制终端分离并进入后台
+daemon = True
 # 要写入的pid文件路径
 pidfile = None
 # gunicorn编写的文件权限掩码
@@ -44,6 +47,7 @@ group = None
 # 存储临时请求数据的目录,请求被读取后可能很快就会消失
 tmp_upload_dir = None
 
+
 # Logging
 # logfile - The path to a log file to write to.
 # A path string. "-" means log to stdout.
@@ -53,20 +57,25 @@ errorlog = '-'
 loglevel = 'info'
 accesslog = '-'
 
+
 # Process naming
 # 与setproctitle一起使用，进程命名，多个gunicorn时用以区分
 proc_name = None
+
 
 # Server hooks
 # server钩子
 def post_fork(server, worker):
     server.log.info("Worker spawned (pid: %s)", worker.pid)
 
+
 def pre_fork(server, worker):
     pass
 
+
 def pre_exec(server):
     server.log.info("Forked child, re-executing.")
+
 
 def when_ready(server):
     server.log.info("Server is ready. Spwawning workers")
