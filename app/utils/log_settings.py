d773b982a319889ab88df08c11434fa36f76e868
chdxia <chdxiamail@gmail.com>
2022-06-21 14:37:09 +0800

    弃用内置logging模块，使用loguru

--- app/utils/log_settings.py
+++ app/utils/log_settings.py
@@ -18,23 +18,3 @@ log_path_all = os.path.join(log_path, f'{time.strftime("%Y-%m-%d")}_log.log')
 
 # rotation日志分割,retention保留时间,enqueue异步写入
 logger.add(log_path_all, rotation="00:00", retention="5 days", enqueue=True)
-
-
-""" logger = logging.getLogger()
-logger.setLevel(logging.INFO)
-
-
-fh = logging.FileHandler(filename=f'./logs/{time.strftime("%Y-%m-%d")}_log.log')
-
-
-formatter = logging.Formatter("%(asctime)s | %(levelname)s     | %(module)s:%(funcName)s:%(lineno)d - %(message)s")
-
-
-fh.setFormatter(formatter)
-
-
-#将日志输出至文件
-logger.addHandler(fh)
-
-
-logger = logging.getLogger(__name__) """
