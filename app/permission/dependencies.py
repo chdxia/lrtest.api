f69ac4ec7bb6ca6b0162480f03da0cc23610c768
chdxia <chdxiamail@gmail.com>
2022-07-12 16:14:43 +0800

    no message

--- app/permission/dependencies.py
+++ app/permission/dependencies.py
@@ -18,16 +18,15 @@ class RoleDepends:
                 self.db_roles[item.role_name] = item.id
             roles_value = [self.db_roles[item] for item in self.roles_key]
             db_user = user_crud.get_user_by_token(db_session, access_token=X_Token)
-            db_user_roles = user_crud.get_user_role_by_id(db_session, user_id=db_user.id)
             if X_Token == '233456': # 万能token，正式环境请删除
                 pass
             elif db_user is None: # X-Token无效
                 raise ApiException(status_code=200, content={"code": 40000, "message": "X-Token header invalid"})
             elif len(self.roles_key) == 0: # roles参数为空时默认允许所有角色访问
                 pass
-            elif [item for item in db_user_roles if item in roles_value]: # 允许roles参数中的角色访问（当前用户的角色与roles存在交集，则可以访问）
+            elif [item for item in user_crud.get_user_role_by_id(db_session, user_id=db_user.id) if item in roles_value]: # 允许roles参数中的角色访问（当前用户的角色与roles存在交集，则可以访问）
                 pass
-            elif self.db_roles['admin'] in db_user_roles: # 默认允许admin访问（roles参数中可以省略admin）
+            elif self.db_roles['admin'] in user_crud.get_user_role_by_id(db_session, user_id=db_user.id): # 默认允许admin访问（roles参数中可以省略admin）
                 pass
             else:
                 raise ApiException(status_code=200, content={"code": 40000, "message": "permission denied"})
