b040df60be2c7f70ee4049cda10d62e405ad0a66
chdxia <chdxiamail@gmail.com>
2022-07-24 04:00:51 +0800

    学习tortoise-orm，将sqlalchemy的crud操作改为tortoise-orm的crud操作

--- app/crud/user_crud.py
+++ app/crud/user_crud.py
@@ -1,33 +1,32 @@
 import uuid
-from sqlalchemy import or_, and_
-from sqlalchemy.orm import Session
-from ..models import models
+from tortoise.query_utils import Q
+from ..models.model import User, UserRole
 from ..schemas import user_schemas
 from ..lib import str_to_sha256
 
 
-def get_user_by_id(db_session: Session, user_id: int):
-    '''根据id查询用户'''
-    return db_session.query(models.User).filter(models.User.id == user_id).first()
+# 根据id查询用户
+async def get_user_by_id(user_id: int):
+    return User.filter(id=user_id).first()
 
 
-def get_user_by_account(db_session: Session, account: str):
-    '''根据账号查询用户'''
-    return db_session.query(models.User).filter(models.User.account == account).first()
+# 根据账号查询用户
+async def get_user_by_account(account: str):
+    return User.filter(account=account).first()
 
 
-def get_user_by_email(db_session: Session, email: str):
-    '''根据邮箱查询用户'''
-    return db_session.query(models.User).filter(models.User.email == email).first()
+# 根据邮箱查询用户
+async def get_user_by_email(email: str):
+    return User.filter(email=email).first()
 
 
-def get_user_by_token(db_session: Session, access_token: str):
-    '''根据token查询用户'''
-    return db_session.query(models.User).filter(models.User.access_token == access_token).first()
+# 根据token查询用户
+async def get_user_by_token(access_token: str):
+    return User.filter(access_token=access_token).first()
 
 
-def get_users(
-    db_session: Session,
+# 查询用户
+async def get_users(
     account: str|None=None,
     user_name: str|None=None,
     email: str|None=None,
@@ -36,71 +35,56 @@ def get_users(
     status: bool|None=None,
     sort: str|None = '+create_time'
 ):
-    '''查询用户'''
-    return db_session.query(models.User).filter(
-        or_(models.User.account.like(f'%{account}%'), account is None),
-        or_(models.User.user_name.like(f'%{user_name}%'), user_name is None),
-        or_(models.User.email.like(f'%{email}%'), email is None),
-        or_(models.User.access_token == access_token, access_token is None),
-        or_(models.User.id.in_(list(map(lambda item: item.user_id, db_session.query(models.UserRole).filter(models.UserRole.role_id == role_id).all()))), role_id is None),
-        or_(models.User.status == status, status is None)
-    ).order_by(
-        and_(models.User.create_time.asc(), sort == '+create_time'),
-        and_(models.User.create_time.desc(), sort == '-create_time'),
-        and_(models.User.update_time.asc(), sort == '+update_time'),
-        and_(models.User.update_time.desc(), sort == '-update_time')
-    ).all()
+    return User.filter(Q(
+        Q(account__contains = account),
+        Q(user_name__contains = user_name),
+        Q(email__contains = email),
+        Q(access_token = access_token),
+        Q(id__in = list(map(lambda item: item.user_id, UserRole.filter(id = role_id).all()))),
+        Q(status = status)
+    )).order_by(sort).all()
 
 
-def create_user(db_session: Session, user: user_schemas.UserCreate):
-    '''新增用户'''
-    db_user = models.User(
+# 新增用户
+async def create_user(user: user_schemas.UserCreate):
+    db_user = User.create(
         account = user.account,
         user_name = user.user_name,
         email = user.email,
         password = str_to_sha256(user.password),
         status = user.status
     )
-    db_session.add(db_user)
-    db_session.commit()
-    db_session.refresh(db_user)
     if user.roles:
         for item in user.roles:
-            db_user_role = models.UserRole(user_id = db_user.id, role_id = item)
-            db_session.add(db_user_role)
-    db_session.commit()
+            UserRole.create(user_id=db_user.id, role_id=item)
     return db_user
 
 
-def update_user(db_session: Session, user:user_schemas.UserUpdate, user_id: int):
-    '''修改用户'''
-    db_session.query(models.UserRole).filter(models.UserRole.user_id == user_id).delete()
-    db_user = db_session.query(models.User).filter(models.User.id == user_id).first()
-    db_user.account = user.account
-    db_user.user_name = user.user_name  
-    db_user.email = user.email
+# 修改用户
+async def update_user(user:user_schemas.UserUpdate, user_id: int):
+    UserRole.filter(user_id=user_id).delete()
+    db_user = User.filter(id=user_id).update(
+        account = user.account,
+        user_name = user.user_name,
+        email = user.email,
+        status = user.status
+    )
     if user.password:
-        db_user.password = str_to_sha256(user.password)
-    db_user.status = user.status
+        db_user = User.filter(id=user_id).update(password = str_to_sha256(user.password))
     if user.roles:
         for item in user.roles:
-            db_user_role = models.UserRole(user_id = user_id, role_id = item)
-            db_session.add(db_user_role)
-    db_session.commit()
-    db_session.refresh(db_user)
+            UserRole.create(user_id=user_id, role_id=item)
     return db_user
 
 
-def delete_user(db_session: Session, user_id: int):
-    '''删除用户'''
-    db_user_role = db_session.query(models.UserRole).filter(models.UserRole.user_id == user_id).all()
-    db_user = db_session.query(models.User).filter(models.User.id == user_id).first()
-    [db_session.delete(item) for item in db_user_role]
-    db_session.delete(db_user)
-    db_session.commit()
+# 删除用户
+async def delete_user(user_id: int):
+    UserRole.filter(user_id=user_id).delete()
+    User.filter(id=user_id).delete()
 
 
-def update_token(db_session: Session, user_id: int|None=None, access_token: str|None=None):
+# 更新token
+async def update_token(user_id: int|None=None, access_token: str|None=None):
     '''
     更新token
 
@@ -109,13 +93,8 @@ def update_token(db_session: Session, user_id: int|None=None, access_token: str|
     没有传入user_id，且传入token时，删除该token值（此处暂未考虑token重复）
     '''
     if user_id:
-        db_user = db_session.query(models.User).filter(models.User.id == user_id).first()
         token = uuid.uuid4()
-        db_user.access_token = token
-        db_session.commit()
+        User.filter(id=user_id).update(access_token=token)
         return token
     elif access_token:
-        db_user = db_session.query(models.User).filter(models.User.access_token == access_token).first()
-        if db_user:
-            db_user.access_token = None
-            db_session.commit()
+        User.filter(access_token=access_token).update(access_token = None)
