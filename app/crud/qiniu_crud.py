770645ce4fee7cef7932a375f6052efeef53b294
chdxia <chdxiamail@gmail.com>
2022-07-24 03:58:57 +0800

    crud改为async异步操作

--- app/crud/qiniu_crud.py
+++ app/crud/qiniu_crud.py
@@ -8,7 +8,7 @@ access_key = get_qiniu_config()['access_key']
 secret_key = get_qiniu_config()['secret_key']
 
 
-def get_qiniu_upload_token():
+async def get_qiniu_upload_token():
     '''生成七牛upload_token'''
     policy = {
         "callbackUrl": get_qiniu_config()['callback_url'],
@@ -18,11 +18,11 @@ def get_qiniu_upload_token():
     return qiniu.Auth(access_key, secret_key).upload_token(bucket=bucket, key=None, policy=policy)
 
 
-def get_qiniu_list():
+async def get_qiniu_list():
     '''获取七牛文件列表'''
     return qiniu.BucketManager(qiniu.Auth(access_key, secret_key)).list(bucket=bucket)
 
 
-def delete_qiniu_files(keys=list):
+async def delete_qiniu_files(keys=list):
     '''批量删除文件'''
     return qiniu.BucketManager(qiniu.Auth(access_key, secret_key)).batch(qiniu.build_batch_delete(bucket=bucket, keys=keys))
