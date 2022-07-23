a7c735557d98bf78d1ec258b526665c831679b86
chdxia <chdxiamail@gmail.com>
2022-07-24 04:04:06 +0800

    修改mysql_url格式

--- app/lib/config.py
+++ app/lib/config.py
@@ -21,21 +21,27 @@ except IOError:
 def get_api_route_depends():
     '''获取api_route_depends'''
     try:
-        api_route_depends = config_data["api_route_depends"]
+        api_route_depends = config_data['api_route_depends']
         return api_route_depends
     except KeyError:
         logger.error('api_route_depends config error!!!')
 
 
-def get_mysql_url():
-    '''获取mysql_database_url'''
+def get_mysql_credentials():
+    '''获取mysql_database_credentials'''
     try:
-        host = config_data["mysql"]["host"]
-        port = config_data["mysql"]["port"]
-        user = quote(config_data["mysql"]["user"])
-        password = quote(config_data["mysql"]["password"])
-        database = config_data["mysql"]["database"]
-        return f'mysql+pymysql://{user}:{password}@{host}:{port}/{database}?charset=utf8'
+        host = config_data['mysql']['host']
+        port = config_data['mysql']['port']
+        user = quote(config_data['mysql']['user'])
+        password = quote(config_data['mysql']['password'])
+        database = config_data['mysql']['database']
+        return {
+            'host': host,
+            'port': port,
+            'user': user,
+            'password': password,
+            'database': database
+        }
     except KeyError:
         logger.error('mysql config error!!!')
 
@@ -43,17 +49,17 @@ def get_mysql_url():
 def get_qiniu_config():
     '''获取七牛配置'''
     try:
-        bucket = config_data["qiniu"]["bucket"]
-        access_key = config_data["qiniu"]["access_key"]
-        secret_key = config_data["qiniu"]["secret_key"]
-        callback_url = config_data["qiniu"]["callback_url"]
-        external_link_base = config_data["qiniu"]["external_link_base"]
+        bucket = config_data['qiniu']['bucket']
+        access_key = config_data['qiniu']['access_key']
+        secret_key = config_data['qiniu']['secret_key']
+        callback_url = config_data['qiniu']['callback_url']
+        external_link_base = config_data['qiniu']['external_link_base']
         return {
-            "bucket": bucket,
-            "access_key": access_key,
-            "secret_key": secret_key,
-            "callback_url": callback_url,
-            "external_link_base": external_link_base
+            'bucket': bucket,
+            'access_key': access_key,
+            'secret_key': secret_key,
+            'callback_url': callback_url,
+            'external_link_base': external_link_base
         }
     except KeyError:
         logger.error('qiniu config error!!!')
