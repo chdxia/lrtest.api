e684e7f44979208a93c3f0abd3aba8b786f3d7f6
chdxia <chdxiamail@gmail.com>
2022-07-24 14:39:37 +0800

    根据tortoise-orm修改api部分

--- app/api/qiniu.py
+++ app/api/qiniu.py
@@ -18,7 +18,7 @@ async def read_qiniu_files():
     file_list = sorted(file_list, key= lambda item: item['putTime'])
     file_list.reverse() # 列表按照上传时间降序排列
     url_list = list(map(lambda item: url_base + '/' + item['key'], file_list)) # 只提取列表信息中的url
-    return {"code": 20000, "message": "success", "data": url_list}
+    return {"code": 200, "message": "success", "data": url_list}
 
 
 @router.delete('/files', summary='批量删除文件', dependencies=[Depends(role_depends('admin'))])
@@ -26,16 +26,16 @@ async def delete_qiniu_files(body: list):
     delete_files = qiniu_crud.delete_qiniu_files(keys=list(map(lambda item: item[len(get_qiniu_config()['external_link_base']) + 1:], body)))
     if delete_files[0]:
         if delete_files[0][0]['code'] == 200:
-            return {"code": 20000, "message": "success"}
+            return {"code": 200, "message": "success"}
     else:
         raise HTTPException(status_code=400, detail='Delete failed')
 
 
 @router.post('/callback', summary='七牛回调')
 async def qiniu_callback(body: dict):
-    return {"code": 20000, "message": "success", "data": dict({"key": body['key']})}
+    return {"code": 200, "message": "success", "data": dict({"key": body['key']})}
 
 
 @router.get('/upload/token', summary='获取七牛token', dependencies=[Depends(role_depends())])
 async def get_qiniu_upload_token():
-    return {"code": 20000, "message": "success", "data": dict({"token": qiniu_crud.get_qiniu_upload_token()})}
+    return {"code": 200, "message": "success", "data": dict({"token": qiniu_crud.get_qiniu_upload_token()})}
--- app/api/users.py
+++ app/api/users.py
@@ -1,7 +1,5 @@
 import re
 from fastapi import APIRouter, HTTPException, Header, Depends
-from sqlalchemy.orm import Session
-from ..database.mysql import get_mysql_db
 from ..crud import user_crud
 from ..schemas import user_schemas
 from ..dependencies import role_depends
@@ -22,64 +20,63 @@ async def get_users(
     status: bool|None=None,
     page: int=1,
     limit: int=10,
-    sort: str|None='+create_time',
-    db_session: Session=Depends(get_mysql_db)
+    sort: str|None='+create_time'
 ):
-    db_user = user_crud.get_users(db_session, account=account, user_name=user_name, email=email, role_id=role_id, status=status, sort=sort)
+    db_user = user_crud.get_users(account=account, user_name=user_name, email=email, role_id=role_id, status=status, sort=sort)
     paginated_users = list(db_user)[(page-1)*limit:(page-1)*limit+limit]
-    return {"code": 20000, "message": "success", "data": dict({"total":len(list(db_user)), "users":paginated_users})}
+    return {"code": 200, "message": "success", "data": dict({"total":len(list(db_user)), "users":paginated_users})}
 
 
 @router.post('', response_model=user_schemas.UserResponse, summary='新增用户', dependencies=[Depends(role_depends('admin'))])
-async def create_user(user: user_schemas.UserCreate, db_session: Session=Depends(get_mysql_db)):
+async def create_user(user: user_schemas.UserCreate):
     if re.fullmatch(r'^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$', user.account): # 账号正则，不能使用邮箱
         raise HTTPException(status_code=400, detail='Account is incorrect')
     if not re.fullmatch(r'^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$', user.email): # 邮箱正则
         raise HTTPException(status_code=400, detail='Email is incorrect')
-    if user_crud.get_user_by_account(db_session, account=user.account): # 验证账号是否已存在
-        raise HTTPException(status_code=400, detail='Account already existed')
-    if user_crud.get_user_by_email(db_session, email=user.email): # 验证邮箱是否已存在
-        raise HTTPException(status_code=400, detail='Email already existed')
-    return {"code": 20000, "message": "success", "data": user_crud.create_user(db_session, user)}
+    if user_crud.get_user(account=user.account): # 验证账号是否已存在
+        raise HTTPException(status_code=400, detail=f'Account "{user.account}" already existed')
+    if user_crud.get_user(email=user.email): # 验证邮箱是否已存在
+        raise HTTPException(status_code=400, detail=f'Email "{user.email}" already existed')
+    return {"code": 201, "message": "success", "data": user_crud.create_user(user)}
 
 
 @router.get('/info', response_model=user_schemas.UserResponse, summary='查询当前用户信息', dependencies=[Depends(role_depends())])
-async def get_info(X_Token: str = Header(...), db_session: Session=Depends(get_mysql_db)):
-    db_user = user_crud.get_user_by_token(db_session, access_token=X_Token)
+async def get_info(X_Token: str = Header(...)):
+    db_user = user_crud.get_user(access_token=X_Token)
     if db_user is None:
-        raise HTTPException(status_code=404, detail='User not found')
-    return {"code": 20000, "message": "success", "data": db_user}
+        raise HTTPException(status_code=400, detail='X-Token header invalid')
+    return {"code": 200, "message": "success", "data": db_user}
 
 
 @router.get('/{user_id}', response_model=user_schemas.UserResponse, summary='根据id查询用户', dependencies=[Depends(role_depends())])
-async def read_user(user_id: int, db_session: Session=Depends(get_mysql_db)):
-    db_user = user_crud.get_user_by_id(db_session, user_id)
+async def read_user(user_id: int):
+    db_user = user_crud.get_user(user_id=user_id)
     if db_user is None:
         raise HTTPException(status_code=404, detail='User not found')
-    return {"code": 20000, "message": "success", "data": db_user}
+    return {"code": 200, "message": "success", "data": db_user}
 
 
 @router.put('/{user_id}', response_model=user_schemas.UserResponse, summary='修改用户', dependencies=[Depends(role_depends('admin'))])
-async def update_user(user_id: int, user: user_schemas.UserUpdate, db_session:Session=Depends(get_mysql_db)):
-    db_user = user_crud.get_user_by_id(db_session, user_id)
+async def update_user(user_id: int, user: user_schemas.UserUpdate):
+    db_user = user_crud.get_user(user_id=user_id)
     if db_user is None:
-        raise HTTPException(status_code=404, detail='User not found')
+        raise HTTPException(status_code=404, detail=f'User "{user.account}" not found')
     if re.fullmatch(r'^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$', user.account):
         raise HTTPException(status_code=400, detail='Account is incorrect')
     if not re.fullmatch(r'^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$', user.email):
         raise HTTPException(status_code=400, detail='Email is incorrect')
-    db_user_account = user_crud.get_user_by_account(db_session, account=user.account)
+    db_user_account = user_crud.get_user(account=user.account)
     if db_user_account and db_user_account != db_user:
-        raise HTTPException(status_code=400, detail='Account already existed')
-    db_user_email = user_crud.get_user_by_email(db_session, email=user.email)
+        raise HTTPException(status_code=400, detail=f'Account "{user.account}" already existed')
+    db_user_email = user_crud.get_user(email=user.email)
     if db_user_email and db_user_email != db_user:
-        raise HTTPException(status_code=400, detail='Email already existed')
-    return {"code": 20000, "message": "success", "data": user_crud.update_user(db_session, user, user_id)}
+        raise HTTPException(status_code=400, detail=f'Email "{user.email}" already existed')
+    return {"code": 200, "message": "success", "data": user_crud.update_user(user, user_id)}
 
 
 @router.delete('/{user_id}', summary='删除用户', dependencies=[Depends(role_depends('admin'))])
-async def delete_user(user_id: int, db_session:Session=Depends(get_mysql_db)):
-    if user_crud.get_user_by_id(db_session, user_id) is None:
+async def delete_user(user_id: int):
+    if user_crud.get_user(user_id=user_id) is None:
         raise HTTPException(status_code=404, detail='User not found')
-    user_crud.delete_user(db_session, user_id)
+    user_crud.delete_user(user_id)
     return {"code": 20000, "message": "success"}
